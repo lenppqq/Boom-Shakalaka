94c9a7273c7e4fcf48d4700b56706cb80c9dab7c
Peng Liang <liangp@purdue.edu>
2016-02-02 10:57:23 -0500

    Create start point

---
+++ src/com/cs490/boom/BoomShakalaka.java
@@ -0,0 +1,12 @@
+package com.cs490.boom;
+
+/**
+ * This class contain the main method as the start point
+ * @author Len
+ *
+ */
+public class BoomShakalaka {
+	public void main (String[] args) {
+		
+	}
+}
