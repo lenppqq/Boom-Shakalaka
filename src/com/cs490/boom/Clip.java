e68cdd9857ca607855681d9cbafa76e69dcb340b
mylzsd <liu863@purdue.edu>
2016-02-24 16:06:49 -0500

    point & clip

---
+++ src/com/cs490/boom/Clip.java
@@ -0,0 +1,14 @@
+package com.cs490.boom;
+
+public class Clip {
+
+	private int start;
+	private int duration;
+	private int musicId;
+	
+	public Clip(int start, int duration, int musicId) {
+		this.start = start;
+		this.duration = duration;
+		this.musicId = musicId;
+	}
+}
--- src/com/cs490/boom/Point.java
+++ src/com/cs490/boom/Point.java
@@ -1,21 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cs490.boom;
 
-/**
- *
- * @author Len
- */
+
 public class Point {
-    public int startPosition;
-    public int duration;
-    public int tags;
-    public int priority;
-    
-    public Point() {
-        
-    }
+    private int start;
+	private int duration;
+	private int tag;
+	private int preference;
+
+	public Point(int start, int duration, int tag, int preference) {
+		this.start = start;
+		this.duration = duration;
+		this.tag = tag;
+		this.preference = preference;
+	}
+
+	public void setStart(start) {
+		this.start = start;
+	}
+
+	public int getStart() {
+		return start;
+	}
+
+	public void setDuration(duration) {
+		this.duration = duration;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setTag(tag) {
+		this.tag = tag;
+	}
+
+	public int getTag() {
+		return tag;
+	}
+
+	public void setPreference(preference) {
+		this.preference = preference;
+	}
+
+	public int getPreference() {
+		return preference;
+	}
 }
