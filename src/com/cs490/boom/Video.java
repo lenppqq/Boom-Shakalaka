2a8010599db5fbd5f0c857d19096680dc916220d
mylzsd <liu863@purdue.edu>
2016-02-24 16:43:19 -0500

    Video PUBLIC!!!

--- src/com/cs490/boom/Video.java
+++ src/com/cs490/boom/Video.java
@@ -4,22 +4,22 @@ import java.util.ArrayList;
 
 public class Video {
 	//basic information
-	private final int videoId;
-	private final String name;
-	private final String path;
-	private final long size;
+	public final int videoId;
+	public final String name;
+	public final String path;
+	public final int size;
 
 	//preference information
-	private int groupId;
-	private int preference;
-	private int tag;
-	private int start;		//in ms
-	private int duration;	//in ms
+	public int groupId;
+	public int preference;
+	public int tag;
+	public int start;		//in ms
+	public int duration;	//in ms
 
 	//critical points
-	private ArrayList<Point> points;
+	public ArrayList<Point> points;
 	
-	public Video(int videoId, String name, String path, long size, int duration) {
+	public Video(int videoId, String name, String path, int size, int duration) {
 		this.videoId = videoId;
 		this.name = name;
 		this.path = path;
@@ -30,69 +30,5 @@ public class Video {
 		start = 0;
 		this.duration = duration;
 		points = new ArrayList<Point>();
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public String getPath() {
-		return path;
-	}
-	
-	public long getSize() {
-		return size;
-	}
-
-	public void setGroupId(int groupId) {
-		this.groupId = groupId;
-	}
-
-	public int getGroupId() {
-		return groupId;
-	}
-
-	public void setPreference(int preference) {
-		this.preference = preference;
-	}
-
-	public int getPreference() {
-		return preference;
-	}
-	
-	public void setTag(int tag) {
-		this.tag = tag;
-	}
-
-	public int getTag() {
-		return tag;
-	}
-
-	public void setStart(int start) {
-		this.start = start;
-	}
-
-	public int getStart() {
-		return start;
-	}
-
-	public void setDuration(int duration) {
-		this.groupId = groupId;
-	}
-
-	public int getDuration() {
-		return duration;
-	}
-
-	public ArrayList<Point> getPoints() {
-		return points;
-	}
-
-	public void addPoint(Point point) {
-		points.add(point);
-	}
-
-	public void deletePoint(Point point) {
-		points.remove(points.indexOf(point));
 	}
 }
