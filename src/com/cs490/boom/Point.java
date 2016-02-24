100c131b65d38574972d6fa056aae3b52f1e104e
Peng Liang <lenppqq@163.com>
2016-02-24 16:08:20 -0500

    clear compile err for Point

--- src/com/cs490/boom/Point.java
+++ src/com/cs490/boom/Point.java
@@ -1,48 +1,48 @@
 package com.cs490.boom;
 
-
 public class Point {
+
     private int start;
-	private int duration;
-	private int tag;
-	private int preference;
+    private int duration;
+    private int tag;
+    private int preference;
 
-	public Point(int start, int duration, int tag, int preference) {
-		this.start = start;
-		this.duration = duration;
-		this.tag = tag;
-		this.preference = preference;
-	}
+    public Point(int start, int duration, int tag, int preference) {
+        this.start = start;
+        this.duration = duration;
+        this.tag = tag;
+        this.preference = preference;
+    }
 
-	public void setStart(start) {
-		this.start = start;
-	}
+    public void setStart(int start) {
+        this.start = start;
+    }
 
-	public int getStart() {
-		return start;
-	}
+    public int getStart() {
+        return start;
+    }
 
-	public void setDuration(duration) {
-		this.duration = duration;
-	}
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
 
-	public int getDuration() {
-		return duration;
-	}
+    public int getDuration() {
+        return duration;
+    }
 
-	public void setTag(tag) {
-		this.tag = tag;
-	}
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
 
-	public int getTag() {
-		return tag;
-	}
+    public int getTag() {
+        return tag;
+    }
 
-	public void setPreference(preference) {
-		this.preference = preference;
-	}
+    public void setPreference(int preference) {
+        this.preference = preference;
+    }
 
-	public int getPreference() {
-		return preference;
-	}
+    public int getPreference() {
+        return preference;
+    }
 }
